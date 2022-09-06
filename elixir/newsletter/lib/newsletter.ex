0c3c7955ab2e1522d4b5b2c560044eac6e95a858
Mark Keele <mark@keele.codes>
2022-09-06 10:49:46 -0400

    add newsletter solution

---
+++ elixir/newsletter/lib/newsletter.ex
@@ -0,0 +1,28 @@
+defmodule Newsletter do
+  def read_emails(path) do
+    path |> File.read!() |> String.split("\n", trim: true)
+  end
+
+  def open_log(path) do
+    File.open!(path, [:write])
+  end
+
+  def log_sent_email(pid, email) do
+    IO.puts(pid, email)
+  end
+
+  def close_log(pid) do
+    File.close(pid)
+  end
+
+  def send_newsletter(emails_path, log_path, send_fun) do
+    log_pid = open_log(log_path)
+
+    emails_path
+    |> read_emails()
+    |> Enum.each(fn email ->
+      if send_fun.(email) == :ok, do: log_sent_email(log_pid, email)
+    end)
+    |> then(fn :ok -> close_log(log_pid) end)
+  end
+end
